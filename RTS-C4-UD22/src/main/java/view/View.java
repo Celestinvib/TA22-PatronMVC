bb30d3b69cd3097cfefca199d0596c2fb684d9b6
pbesora <pau_besora@hotmail.com>
2022-08-08 11:18:05 +0200

    Comments on View

--- RTS-C4-UD22/src/main/java/view/View.java
+++ RTS-C4-UD22/src/main/java/view/View.java
@@ -10,34 +10,47 @@ import javax.swing.JButton;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.BevelBorder;
 import java.awt.BorderLayout;
+import javax.swing.JSeparator;
 
 public class View extends JFrame{
 
 	public JFrame frame;
 	public JTable table;
-	public JPanel panel;
+	public JPanel panelTable;
 	public JPanel panelButtons;
 	
 	public JButton btnUpdate;
 	public JButton btnDelete;
 	public JButton btnCreate;
 
+	JPanel panelUpdate;
+	JPanel panelCreate;
+	private JPanel panel;
+	private JTable tableUpdate;
+	JButton btnUpdateData;
+	private JButton btnCreateData;
+	
 	/**
 	 * Create the application.
 	 */
 	public View() {
-		
+		/**
+		 * Window frame
+		 */
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.getContentPane().setLayout(null);
 		
-		panel = new JPanel();
-		panel.setBounds(10, 11, 414, 239);
-		frame.getContentPane().add(panel);
-		panel.setLayout(new BorderLayout(0, 0));
+		/**
+		 * Main panel with the table and CRUD options
+		 */
+		panelTable = new JPanel();
+		panelTable.setBounds(10, 10, 415, 240);
+		frame.getContentPane().add(panelTable);
+		panelTable.setLayout(new BorderLayout(0, 0));
 		
 		panelButtons = new JPanel();
-		panel.add(panelButtons, BorderLayout.SOUTH);
+		panelTable.add(panelButtons, BorderLayout.SOUTH);
 		panelButtons.setLayout(new GridLayout(1, 0, 10, 0));
 		
 		btnUpdate = new JButton("Actualizar");
@@ -54,7 +67,39 @@ public class View extends JFrame{
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 
-		panel.add(new JScrollPane(table), BorderLayout.CENTER);
+		panelTable.add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		
+		/**
+		 * Update panel
+		 */
+		panelUpdate = new JPanel();
+		panelUpdate.setBounds(10, 10, 415, 240);
+		frame.getContentPane().add(panelUpdate);
+		panelUpdate.setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBounds(10, 0, 395, 56);
+		panelUpdate.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		tableUpdate = new JTable();
+		panel.add(tableUpdate);
+		
+		btnUpdateData = new JButton("Actualizar");
+		btnUpdateData.setBounds(10, 210, 90, 20);
+		panelUpdate.add(btnUpdateData);
+		
+		/**
+		 * Create panel
+		 */
+		panelCreate = new JPanel();
+		panelCreate.setBounds(10, 10, 415, 240);
+		frame.getContentPane().add(panelCreate);
+		panelCreate.setLayout(null);
+		
+		btnCreateData = new JButton("Crear");
+		btnCreateData.setBounds(10, 210, 90, 20);
+		panelCreate.add(btnCreateData);
 	}
-
 }
