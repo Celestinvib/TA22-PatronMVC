e4b206481ae21f329ea740b403ca2e0bbfc7e7cb
Celestinvib <celestinvib@gmail.com>
2022-08-08 11:37:55 +0200

    Ex1 Video v0.2

--- RTS-C4-UD22/src/main/java/model/Video.java
+++ RTS-C4-UD22/src/main/java/model/Video.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Video {
 	/** Attributes */
 	private int id;
@@ -79,6 +83,39 @@ public class Video {
 	 */
 	public void setCli_id(int cli_id) {
 		this.cli_id = cli_id;
+	}
+	
+	
+	public void tableStructureCreation(SQLConnection connection) {		
+		String structure  = "create table videos ("
+				+ "	id int(11) NOT NULL AUTO_INCREMENT,"
+				+ "    title varchar(250) DEFAULT NULL,"
+				+ "    director varchar(250) DEFAULT NULL,"
+				+ "    cli_id int(11) DEFAULT NULL,"
+				+ "    PRIMARY KEY (id),"
+				+ "    CONSTRAINT videos_fk FOREIGN KEY (cli_id) REFERENCES cliente (id)"
+				+ ");";
+		connection.insertData(structure);
+	}
+	
+	public void selectAllVideos(SQLConnection connection) {		
+		String query = "select * from videos;";
+//	    connection.insertData(query);
+	}
+	
+	public void selectOneVideo(SQLConnection connection, String id) {		
+		String query = "select * from videos where id="+id+";";
+//	    connection.insertData(query);
+	}
+	
+	public void updateVideo(SQLConnection connection, String id, String title ,String director,String cli_id ) {		
+		String query = "select * from videos where id="+id+";";
+	    connection.insertData( query);
+	}
+	
+	public void deleteVideo(SQLConnection connection,String id) {
+		String query = "delete from videos where id="+id+";";
+	    connection.insertData(query);
 	}
 	
 	
