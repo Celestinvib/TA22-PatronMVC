2e44a3e8f74c5d269c13fe9051fcf5a34c288d8b
Celestinvib <celestinvib@gmail.com>
2022-08-08 13:08:47 +0200

    Ex1 Video v1.1

--- RTS-C4-UD22/src/main/java/model/Video.java
+++ RTS-C4-UD22/src/main/java/model/Video.java
@@ -1,12 +1,13 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Video {
 	
+	/**
+	 * Method that creates the table structure of the table videos
+	 * @param connection Connection  with the db
+	 */
 	public void tableStructureCreation(SQLConnection connection) {		
 		String structure  = "create table videos ("
 				+ "	id int(11) NOT NULL AUTO_INCREMENT,"
@@ -19,23 +20,61 @@ public class Video {
 		connection.insertData(structure);
 	}
 	
+	/**
+	 * Method that return all the videos stored in the db
+	 * @param connection Connection with the db
+	 * @return all the videos that the select has returned
+	 */
 	public ResultSet selectAllVideos(SQLConnection connection) {		
 		String query = "select * from videos;";
 	    return connection.getValues(query);
 	}
 	
-	public ResultSet selectOneVideo(SQLConnection connection, String id) {		
+	/**
+	 * Method that return a video from the db
+	 * @param connection Connection with the db
+	 * @param id  id of the video from which you want to obtain the fields
+	 * @return the video that the select has returned
+	 */
+	public ResultSet selectOneVideo(SQLConnection connection, int id) {		
 		String query = "select * from videos where id="+id+";";
 		
 	    return connection.getValues(query);
 	}
 	
-	public void updateVideo(SQLConnection connection, String id, String title ,String director,String cli_id ) {		
-		String query = "UPDATE videos set title="+title+", director="+director+" ,cli_id="+cli_id+" where id="+id+";";
+	
+	/**
+	 * Method that insert a new video to the table videos
+	 * @param connection Connection with the db
+	 * @param title  title of the video
+	 * @param director director of the video
+	 * @param cli_id  client owner of the video
+	 */	
+	public void insertVideo(SQLConnection connection,String title ,String director,int cli_id ) {			
+		String query = "INSERT INTO videos (title, director, cli_id) VALUES ('"+title+"','"+director+"',"+cli_id+");";
+	    connection.insertData(query);   
+	}
+	
+	
+	/**
+	 * Method that update an existing video with new fields
+	 * @param connection Connection with the db
+	 * @param id  id of the video
+	 * @param title  new title of the video
+	 * @param director new director of the video
+	 * @param cli_id  new client owner of the video
+	 */	
+	public void updateVideo(SQLConnection connection, int id, String title ,String director,int cli_id ) {		
+		String query = "UPDATE videos set title='"+title+"', director='"+director+"' ,cli_id="+cli_id+" where id="+id+";";
 	    connection.insertData(query);
 	}
 	
-	public void deleteVideo(SQLConnection connection,String id) {
+	/**
+	 * Method that deletes a video from the db
+	 * @param connection Connection with the db
+	 * @param id  id of the video you want to delete
+	 */
+	public void deleteVideo(SQLConnection connection,int id) {
 		String query = "delete from videos where id="+id+";";
 	    connection.insertData(query);
 	}
