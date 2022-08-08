2aeea5e50b2526de43e0cbdf0e167fdab8f599a3
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-08 10:28:32 +0200

    Commit MainApp with the main creating Client, view and controller and initiating the view

--- RTS-C4-UD22/src/main/java/appMain/MainApp.java
+++ RTS-C4-UD22/src/main/java/appMain/MainApp.java
@@ -1,5 +1,18 @@
 package appMain;
+	
+import controller.Controller;
+import model.Client;
+import view.View;
 
 public class MainApp {
-
+	
+	public static void main(String[] args) {
+	
+		Client client = new Client(null, null, null, 0, null);
+		View view = new View();
+		Controller controller = new Controller(client, view);
+		controller.initiateView();
+		
+	}
 }
+
