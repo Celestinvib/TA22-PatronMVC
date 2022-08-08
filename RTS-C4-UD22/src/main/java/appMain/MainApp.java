3f617275af80c54c17029894eee697f2c39e7522
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-08 10:37:39 +0200

    Commit for main comments

--- RTS-C4-UD22/src/main/java/appMain/MainApp.java
+++ RTS-C4-UD22/src/main/java/appMain/MainApp.java
@@ -7,7 +7,10 @@ import view.View;
 public class MainApp {
 	
 	public static void main(String[] args) {
-	
+		
+		/**
+		 * Creation of new client, view and controller, and initiating them
+		 */
 		Client client = new Client(null, null, null, 0, null);
 		View view = new View();
 		Controller controller = new Controller(client, view);
