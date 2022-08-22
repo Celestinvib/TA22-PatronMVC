ebfd7e92a6a1225f7256a669615b0d63a977a097
pbesora <pau_besora@hotmail.com>
2022-08-22 10:05:20 +0200

    ScientistsProjects created

--- CRUDCientificosProyecto/src/main/java/model/ScientistsProjects.java
+++ CRUDCientificosProyecto/src/main/java/model/ScientistsProjects.java
@@ -1,5 +1,73 @@
 package model;
 
-public class ScientistsProjects {
+import java.sql.ResultSet;
 
+public class ScientistsProjects {
+	/**	Method that creates the table scientists_projects
+	 * @param connection Connection with the database
+	 */
+	public void tableStructureCreation(SQLConnection connection) {
+
+		String structure  = "create table scientists_projects (ID int autoincrement, Scientist VARCHAR(8), Project int, "
+				+ "CONSTRAINT FK_DNISciPro FOREIGN KEY (Scientist) REFERENCES Scientist(DNI) ON DELETE CASCADE ON UPDATE CASCADE, "
+				+ "CONSTRAINT FK_IDSciPro FOREIGN KEY (Project) REFERENCES Project (Id) ON DELETE CASCADE ON UPDATE CASCADE, "
+				+ " PRIMARY KEY (ID)"
+				+ ");";
+		connection.insertData(structure);
+		
+		}
+	
+	/**	Method that return all the scientists_projects stored in the database
+	 * @param connection Connection with the database
+	 * @return all the scientists_projects that the select has returned
+	 */
+	public ResultSet selectAllScientisProjects(SQLConnection connection) {		
+		String query = "select * from scientists_projects;";
+		return connection.getValues(query);
+	}
+	
+	/**
+	 * Method that return a project from the database
+	 * @param connection Connection with the database
+	 * @param Id
+	 * @return the scientists_projects that the select has returned
+	 */
+	public ResultSet selectOneScientistsProjects(SQLConnection connection, int Id) {		
+		String query = "select * from scientists_projects where Id='"+Id+"';";
+		return connection.getValues(query);
+	}
+	
+	/**
+	 *  Method that update an existing scientists_projects with new fields
+	 * @param connection
+	 * @param Id
+	 * @param name
+	 * @param hours
+	 */
+	public void updateScientistsProjects(SQLConnection connection, int Id, String scientist, int project) {		
+		String query = "UPDATE scientists_projects set scientist='" + scientist + "', project=" + project + " where Id='"+Id+"';";
+	    connection.insertData(query);
+	}
+	
+	/**
+	 * 
+	 * @param connection
+	 * @param Id
+	 */
+	public void deleteScientistsProjects(SQLConnection connection, int Id) {
+		String query = "delete from scientists_projects where Id="+Id+";";
+	    connection.insertData(query);
+	}
+	
+	
+	/**
+	 * 
+	 * @param connection
+	 * @param scientist
+	 * @param project
+	 */
+	public void insertScientistsProject(SQLConnection connection, String scientist, String project  ) {
+		String query = "INSERT INTO scientists_projects (sientist, project) VALUES ('" + scientist + "','" + project + ");";
+	    connection.insertData(query);
+	}
 }
